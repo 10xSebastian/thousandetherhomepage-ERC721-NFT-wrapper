900179366e21685bf6043c97ca183e1de53b07be
Sebastian Pape <s@pape.io>
2021-08-26 16:17:12 +0200

    update flattened contracts

---
+++ flatten/IKetherHomepage.sol
@@ -0,0 +1,27 @@
+// Root file: contracts/IKetherHomepage.sol
+
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.6;
+pragma experimental ABIEncoderV2;
+
+interface IKetherHomepage {
+
+  struct Ad {
+    address owner;
+    uint x;
+    uint y;
+    uint width;
+    uint height;
+    string link;
+    string image;
+    string title;
+    bool NSFW;
+    bool forceNSFW;
+  }
+
+  function ads(uint _idx) external returns(Ad memory);
+  function setAdOwner(uint _idx, address _newOwner) external;
+  function publish(uint _idx, string memory _link, string memory _image, string memory _title, bool _NSFW) external;
+  
+}
